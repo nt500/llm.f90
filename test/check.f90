07ca21cbe0bf8ea9a8a01255e33ceb16206aad57
Neil Thayamballi <neilthayamballi@gmail.com>
2025-07-30 11:42:04 -0700

    Changed check.f90

--- test/check.f90
+++ test/check.f90
@@ -1,5 +1,22 @@
 program check
-implicit none
+  use, intrinsic :: iso_fortran_env
+  use :: precision_module
+  use :: mixed_type_module
+  implicit none
 
-print *, "fpm test passed"
+  print *, "Running tests..."
+
+contains
+
+  subroutine print_result(test_name, condition)
+    character(len=*), intent(in) :: test_name
+    logical, intent(in) :: condition
+
+    if (condition) then
+      print *, "PASS: ", trim(test_name)
+    else
+      print *, "FAIL: ", trim(test_name)
+    end if
+  end subroutine print_result
+
 end program check
